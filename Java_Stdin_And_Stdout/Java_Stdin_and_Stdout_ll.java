87b21c8d2754734c53cc7dfa654e529655137416
sayadinsaimu <55958502+sayadinsaimu@users.noreply.github.com>
2023-02-22 22:11:30 +0800

    Add files via upload

---
+++ Java_Stdin_And_Stdout/Java_Stdin_and_Stdout_ll.java
@@ -0,0 +1,17 @@
+package Java_Stdin_And_Stdout;
+
+import java.util.Scanner;
+
+public class Java_Stdin_and_Stdout_ll {
+	 public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        int a = scan.nextInt();
+	        int b = scan.nextInt();
+	        int c = scan.nextInt();
+
+	        System.out.println(a);
+	        System.out.println(b);
+	        System.out.println(c);
+	        scan.close();
+	    }
+}
