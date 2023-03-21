4b53a4b143869867683a2e1cd8c9fe6f7e07d5b2
Sayadin Saimu <55958502+sayadinsaimu@users.noreply.github.com>
2023-03-21 23:34:26 +0800

    Update Prime_Checker.java

--- Prime_Checker/Prime_Checker.java
+++ Prime_Checker/Prime_Checker.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
+import static java.lang.System.in;
 
 class Prime {
 	public void checkPrime(int... numbers) {
