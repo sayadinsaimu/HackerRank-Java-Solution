5b486d928829578c36c10e2ecffd2a45a9ff87dd
Sayadin Saimu <55958502+sayadinsaimu@users.noreply.github.com>
2023-03-21 23:33:47 +0800

    Update Prime_Checker.java

--- Prime_Checker/Prime_Checker.java
+++ Prime_Checker/Prime_Checker.java
@@ -22,11 +22,11 @@ class Prime {
 		if (num == 2) {
 			return true;
 		}
-		for (int i = 3; i < (num / 2); i++) {
-			if (num % i == 0) {
-				return false;
-			}
-		}
+		for (int i = 2; i < num ; i++) {
+            		if (num % i == 0) {
+                		return false;
+            		}
+        	}
 		return true;
 	}
 }
