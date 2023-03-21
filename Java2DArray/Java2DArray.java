41ffd11215bd5693c2ce355ae4206669375611cf
Sayadin Saimu <55958502+sayadinsaimu@users.noreply.github.com>
2023-03-21 23:58:21 +0800

    Update Java2DArray.java

--- Java2DArray/Java2DArray.java
+++ Java2DArray/Java2DArray.java
@@ -1,26 +1,37 @@
 package Java2DArray;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 
 public class Java2DArray {
-	public static void main(String[] args) throws IOException {
-		BufferedReader bufferedReader = new BufferedReader(
-										new InputStreamReader(System.in));
-		List<List<Integer>> arr = new ArrayList<>();
-		for(int i = 0; i<6;i++) {
-			String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split("");
-			List<Integer> arrRowItemps = new ArrayList<>();
-			for(int j =0; j<6;j++) {
-				int arrItem = Integer.parseInt(arrRowTempItems[j]);
-				arrRowItemps.add(arrItem);
-			}
-			arr.add(arrRowItemps);
-		}
-		bufferedReader.close();
-	}
+    public static void main(String[] args)
+    {
+        int a[][] = new int[6][6];
+        int maxSum = Integer.MIN_VALUE;
+        try (Scanner scanner = new Scanner(System.in);)
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 6; j++)
+                {
+                    a[i][j] = scanner.nextInt();
+                    if (i > 1 && j > 1)
+                    {
+                        int sum =
+                            a[i][j]
+                            + a[i][j-1]
+                            + a[i][j-2]
+                            + a[i-1][j-1]
+                            + a[i-2][j]
+                            + a[i-2][j-1]
+                            + a[i-2][j-2];
+                        if (sum > maxSum) {maxSum = sum;}
+                    }
+                }
+            }
+        }
+        System.out.println(maxSum);
+    }
 }
+
